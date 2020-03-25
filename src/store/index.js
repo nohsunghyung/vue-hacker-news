3b8faf85463d032f9c526ca0f4f180d65d871f4e
nohsunghyung <nolu8085@naver.com>
2020-03-25 17:42:55 +0900

    feat : 스토어 간격 수정

--- src/store/index.js
+++ src/store/index.js
@@ -6,19 +6,19 @@ import mutations from './mutations';
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
-    state : {
-			news : [],
-			jobs : [],
-			ask : [],
-			user : {},
-			item : {},
-			list : [],
-    },
-    getters : {
-			fechedItem(state){
-				return state.item;
-			}
-    },
-    mutations,
-    actions,
+  state : {
+    news : [],
+    jobs : [],
+    ask : [],
+    user : {},
+    item : {},
+    list : [],
+  },
+  getters : {
+    fechedItem(state){
+      return state.item;
+    }
+  },
+  mutations,
+  actions,
 })
